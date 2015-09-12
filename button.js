508affa5f9f435b2a17d961163de96f2ce1b2466
Nelson Melo <nmelo.cu@gmail.com>
2015-09-12 15:33:04 -0400

    Testing

--- button.js
+++ button.js
@@ -6,7 +6,8 @@ var lifx = new lifxObj(config.lifx_access_token);
 var dash = dash_button("10:ae:60:a3:bf:06"); //address from step above
 dash.on("detected", function (){
 
+    console.log("Toggling all lights");
     lifx.togglePower("all", function(){
-      console.log("Toggling all lights");
+      console.log("Lights toggled");
     });
 });
